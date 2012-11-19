fac229dc11edf6c84c5a050ef6350698c858328f
zachkinstner <zach@aestheticinteractive.com>
2012-11-18 22:52:12 -0500

    Wrote a sample query to check domain classes with Weaver, imagined possible API path representation.

--- Solution/Fabric.Domain/FabricQuery.cs
+++ Solution/Fabric.Domain/FabricQuery.cs
@@ -8,7 +8,29 @@ namespace Fabric.Domain {
 
 		////////////////////////////////////////////////////////////////////////////////////////////////
 		/*--------------------------------------------------------------------------------------------*/
-		public FabricQuery() : base(new Root()) { }
+		public FabricQuery() : base(new Root()) {
+			/*
+			 
+			>>> Get all App 99 users that have a MemberTypeId > 2:
+			
+			var x = Root
+				.OutHasApps.ToApp
+					.Has<App>(a => a.AppId, WeaverFuncHasOp.EqualTo, 99)
+				.InMembersUses.FromMember
+					.As<Member>("m")
+				.OutHasMemberTypeAssign.ToMemberTypeAssign
+				.OutUsesMemberType.ToMemberType
+					.Has<MemberType>(mt => mt.MemberTypeId, WeaverFuncHasOp.GreaterThan, 2)
+					.Back<Member>("m")
+				.OutUsesUser.ToUser;
+			
+			>>> Possible API path:
+			
+			/OutHasApps/Where(AppId;EqualTo;99)/InMembersUse/As(m)/OutHasMemberTypeAssign ...
+				...	/OutUsesMemberType/Where(MemberTypeId|GreaterThan|2)/Back(m)/OutUsesUser
+			
+			*/
+		}
 
 		/*--------------------------------------------------------------------------------------------*/
 		public Root Root { get { return (Root)BaseNode; } }
