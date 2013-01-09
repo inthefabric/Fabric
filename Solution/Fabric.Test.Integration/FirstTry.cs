59b77e820161c3f2b4a6e1d249e29882448a7c93
zachkinstner <zach@aestheticinteractive.com>
2013-01-08 20:57:43 -0500

    Researched Neo4j unit testing, attempted to convert JARs to DLLs, no luck. Updated NuGet packages.  Started initial integration test attempts.

---
+++ Solution/Fabric.Test.Integration/FirstTry.cs
@@ -0,0 +1,28 @@
+﻿using Fabric.Api.Server;
+using Nancy;
+using Nancy.Testing;
+using NUnit.Framework;
+
+namespace Fabric.Test.Integration {
+
+	/*================================================================================================*/
+	[TestFixture]
+	public class FirstTry {
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		[Test]
+		public void Attempt() {
+			var api = typeof(ApiModule);
+			var boot = new DefaultNancyBootstrapper();
+			var browser = new Browser(boot);
+
+			BrowserResponse result = browser.Get("/", b => b.HttpRequest());
+
+			Assert.Fail(result.StatusCode+"");
+		}
+
+	}
+
+}
---
+++ Solution/Fabric.Test.Integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Fabric.Test.Integration")]
+[assembly: AssemblyDescription("")]
