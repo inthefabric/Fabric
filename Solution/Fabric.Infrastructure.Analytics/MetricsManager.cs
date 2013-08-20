c470f9aafdaaa13fda6ae8d474763abbdb033d7a
Zach Kinstner <zach@aestheticinteractive.com>
2013-08-20 14:17:43 -0400

    Started initial Graphite metrics work.

---
+++ Solution/Fabric.Infrastructure.Analytics/MetricsManager.cs
@@ -0,0 +1,24 @@
+﻿using Graphite;
+
+namespace Fabric.Infrastructure.Analytics {
+	
+	/*================================================================================================*/
+	public class MetricsManager {
+
+		private readonly GraphiteUdpClient vGraphite;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public MetricsManager(string pHost, int pPort, string pPrefix) {
+			vGraphite = new GraphiteUdpClient(pHost, pPort, pPrefix);
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public void Record(string pPath, int pValue) {
+			vGraphite.Send(pPath, pValue);
+		}
+
+	}
+
+}
